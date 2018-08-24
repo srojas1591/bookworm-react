fb52d03b300426e1d3f2315010d57854985e42a6
Steven Rojas Chevez <steven.rojas@ibm.com>
2018-08-24 15:14:18 -0600

    axios installation

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoginForm from "../forms/LoginForm";
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login} from '../../actions/auth';
 
 
 class LoginPage extends React.Component {
 
-    submit = data =>{
-        console.log(data);
-    };
+    submit = data => 
+    this.props.login(data).then( () => this.props.history.push ("/") );
 
     render() {
         return (
@@ -18,5 +20,13 @@ class LoginPage extends React.Component {
     }
 }
 
+LoginPage.PropTypes = {
+    history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+    }).isRequired,
+    login: PropTypes.func.isRequired
+};
 
-export default LoginPage;
+
+
+export default connect(null, {login}) (LoginPage);
